692e8eea29f3005f94da1ad4d64bc2bc81c42186
MayconFelipeBianchi <114965402+MayconFelipeBianchi@users.noreply.github.com>
2023-05-10 22:19:51 -0300

    Add files via upload

---
+++ calculadora/src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadora;
+
+/**
+ *
+ * @author aluno
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+    NewClass calc = new NewClass();
+        int resultado1 = calc.somar(2,3);
+        double resultado2 = calc.somar(2.5, 3.5);
+        int resultado3 = calc.somar (2,3,4);
+        
+    }
+}
---
+++ calculadora/src/main/java/com/mycompany/calculadora/NewClass.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.calculadora;
+
+/**
+ *
+ * @author aluno
+ */
+public class NewClass {
+     public int somar (int a, int b){
+         return a + b;
+}
+    
+public double somar (double a, double b){
+return a + b;
+}
+public int somar (int a, int b, int c){
+return a + b + c;
+}
+}
+
